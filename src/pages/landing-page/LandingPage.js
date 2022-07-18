e0afc724bae01c830f894b510c73c8b67c2bd94e
Lucas <77863766+i-am-lucas-almeida@users.noreply.github.com>
2022-07-18 18:25:43 -0300

    Add LandingPage page

---
+++ src/pages/landing-page/LandingPage.js
@@ -0,0 +1,113 @@
+import styles from "../../sass/pages/LandingPage.module.scss";
+
+import LinkButton from "../../components/LinkButton";
+
+import iconCheck from "../../assets/Checkmark.svg";
+import logoAirbnb from "../../assets/Partners/Airbnb.svg";
+import logoBooking from "../../assets/Partners/Booking.svg";
+import logoPlum from "../../assets/Partners/Plum.svg";
+import Logo from "../../components/Logo";
+
+const LandingPage = () => {
+
+    return (
+
+        <div className={styles.container}>
+
+            <nav className={styles.navbar}>
+
+                <Logo link="/" />
+
+            </nav>
+
+            <div className={styles.container__presentation}>
+
+                <div className={styles.container__presentation_content}>
+
+                    <div>
+
+                        <Logo link="/" />
+
+                    </div>
+
+                    <h1>
+                        Explore os melhores camps do planeta.
+                    </h1>
+
+                    <p>
+                        YelpCamp é uma lista com curadoria dos melhores pontos de acampamento do planeta. Viva as melhores experiências ao ar livre.
+                    </p>
+
+                    <ul>
+                        <li>
+                            <img
+                                src={iconCheck}
+                                alt="ícone de check"
+                            />
+                            <p>
+                                Adicione suas sugestões de acampamentos.
+                            </p>
+                        </li>
+
+                        <li>
+                            <img
+                                src={iconCheck}
+                                alt="ícone de check"
+                            />
+                            <p>
+                                Compartilhe reviews e experiências.
+                            </p>
+                        </li>
+
+                        <li>
+                            <img
+                                src={iconCheck}
+                                alt="ícone de check"
+                            />
+                            <p>
+                                Localize o camping mais próximo de você.
+                            </p>
+                        </li>
+                    </ul>
+
+                    <LinkButton
+                        text="Explorar Acampamentos"
+                        link="/home"
+                    />
+
+                    <ul className={styles.partners}>
+                        <li>
+                            <img
+                                src={logoAirbnb}
+                                alt="ícone Airbnb"
+                            />
+                        </li>
+
+                        <li>
+                            <img
+                                src={logoBooking}
+                                alt="ícone Booking"
+                            />
+                        </li>
+
+                        <li>
+                            <img
+                                src={logoPlum}
+                                alt="ícone Plum Guide"
+                            />
+                        </li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+            <div className={styles.container__image}></div>
+
+        </div>
+
+    );
+
+};
+
+export default LandingPage;
