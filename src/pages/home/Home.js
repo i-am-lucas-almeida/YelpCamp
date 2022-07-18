f7b074901da12bb1fb83f444820f264827be9868
Lucas <77863766+i-am-lucas-almeida@users.noreply.github.com>
2022-07-18 18:24:08 -0300

    Add new pages

---
+++ src/pages/home/Home.js
@@ -0,0 +1,24 @@
+import Container from "../../components/Container";
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
+import Search from "./Search";
+
+const Home = () => {
+
+    return (
+
+        <Container>
+
+            <Navbar />
+
+            <Search />
+
+            <Footer />
+
+        </Container>
+
+    );
+
+};
+
+export default Home;
