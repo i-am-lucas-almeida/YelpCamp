591cf0e0169e1261df82545681bed57e5319a7a0
Lucas <77863766+i-am-lucas-almeida@users.noreply.github.com>
2022-07-19 17:32:00 -0300

    Create register page

---
+++ src/pages/register/FormRegister.js
@@ -0,0 +1,169 @@
+import styles from "../../sass/pages/FormSection.module.scss";
+
+import { useState } from "react";
+
+const FormRegister = () => {
+
+    const initialValues = {
+        email: "",
+        username: "",
+        password: "",
+        touched: {
+            email: false,
+            username: false,
+            password: false
+        }
+    };
+
+    const [formData, setFormData] = useState(initialValues);
+
+    const { email, username, password } = formData;
+
+    function onChange(e) {
+
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+
+    }
+
+    function onBlur(e) {
+
+        const { name } = e.target;
+        setFormData({ ...formData, touched: { ...formData.touched, [name]: true } });
+
+    }
+
+    function validate() {
+
+        const errors = {
+            email: "",
+            username: "",
+            password: ""
+        };
+
+        const validEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{3,4}$/i;
+
+        const validUsername = /^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,13}[a-zA-Z0-9]$/;
+
+        if (formData.touched.email && !formData.email.match(validEmail)) {
+
+            errors.email = "Email inválido!";
+
+        }
+
+        if (formData.touched.username && !formData.username.match(validUsername)) {
+
+            errors.username = "Username inválido!";
+
+        }
+
+        if (formData.touched.password && formData.password.length < 8) {
+
+            errors.password = "Senha inválida!";
+
+        }
+
+        return errors;
+
+    }
+
+    const errors = validate();
+
+    function onSubmit(e) {
+
+        e.preventDefault();
+
+        if ((!errors.email) &&
+            (!errors.username) &&
+            (!errors.password)) {
+
+            console.log(formData);
+            setFormData(initialValues);
+
+        }
+
+    }
+
+    return (
+
+        <form onSubmit={onSubmit} className={styles.container__form}>
+
+            <fieldset>
+
+                <label>Email</label>
+
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="email@exemplo.com"
+                    required
+                    value={email}
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    className={errors.email ? styles.error : ""}
+                />
+
+                {errors.email && <small>{errors.email}</small>}
+
+            </fieldset>
+
+            <fieldset>
+
+                <label>Username</label>
+
+                <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="educamargo_91"
+                    maxLength={15}
+                    minLength={5}
+                    size={15}
+                    required
+                    value={username}
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    className={errors.username ? styles.error : ""}
+                />
+
+                {errors.username && <small>{errors.username}</small>}
+
+            </fieldset>
+
+            <fieldset>
+
+                <label>Senha</label>
+
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Senha com 8 caracteres"
+                    minLength={8}
+                    maxLength={30}
+                    size={30}
+                    required
+                    value={password}
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    className={errors.password ? styles.error : ""}
+                />
+
+                {errors.password && <small>{errors.password}</small>}
+
+            </fieldset>
+
+            <button
+                type="submit"
+                className={styles.container__form_button}>
+                Criar uma conta
+            </button>
+
+        </form >
+
+    );
+
+};
+
+export default FormRegister;
---
+++ src/pages/register/Register.js
@@ -0,0 +1,39 @@
+import styles from "../../sass/pages/FormSection.module.scss";
+
+import { Link } from "react-router-dom";
+
+import TestimonialSection from "../../components/TestimonialSection";
+import NavbarForm from "../../components/NavbarForm";
+import FormRegister from "./FormRegister";
+
+const Register = () => {
+
+    return (
+
+        <div className={styles.container}>
+
+            <div className={styles.container__content}>
+
+                <NavbarForm link="/signup" />
+
+                <h1 className={styles.container__title}>
+                    Comece a explorar acampamentos de todo o mundo.
+                </h1>
+
+                <FormRegister />
+
+                <p className={styles.container__answer}>
+                    Já tem uma conta? <Link to="/login">Entrar</Link>
+                </p>
+
+            </div>
+
+            <TestimonialSection />
+
+        </div>
+
+    );
+
+};
+
+export default Register;
