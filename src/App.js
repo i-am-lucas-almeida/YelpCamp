a49208d1fb444eaa11ed64671d7f8d5a7a94c24e
Lucas <77863766+i-am-lucas-almeida@users.noreply.github.com>
2022-07-19 17:32:16 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ScrollToTop from "./utils/ScrollToTop";
 
 import LandingPage from "./pages/landing-page/LandingPage";
 import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 const App = () => {
 
@@ -19,6 +21,8 @@ const App = () => {
 
           <Route path="/" element={<LandingPage />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="/login" element={<Login />} />
 
         </Routes>
 
