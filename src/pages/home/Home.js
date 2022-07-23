6ef7d121b011572b4e20aa9d352dfaa107f5d6d4
Lucas <77863766+i-am-lucas-almeida@users.noreply.github.com>
2022-07-23 18:20:03 -0300

    Update home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,17 +1,47 @@
+import styles from "../../sass/pages/Home.module.scss";
+
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
 import Container from "../../components/Container";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
-import Search from "./Search";
+import SearchCard from "../../components/SearchCard";
+import CampCard from "../../components/CampCard";
+
+import { PageLoading } from "../../components/Loading";
 
 const Home = () => {
+
+    const { documents: data, error, loading } = useFetchDocuments("camps");
 
     return (
 
         <Container>
 
+            {loading && <PageLoading />}
+
             <Navbar />
 
-            <Search />
+            <SearchCard />
+
+            {error && <p>{error}</p>}
+
+            <ul className={styles.list}>
+
+                {data.length > 0 && data.map((item) => (
+
+                    <CampCard
+                        key={item.id}
+                        {...item}
+                    />
+
+                ))}
+
+                {data.length < 0 &&
+                    <h2>Nenhum acampamento cadastrado.</h2>
+                }
+
+            </ul>
 
             <Footer />
 
