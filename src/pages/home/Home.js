dd941373fdea9857b4d3a87e1f409ca7f21b5313
Lucas <77863766+i-am-lucas-almeida@users.noreply.github.com>
2022-07-28 18:08:35 -0300

    Update Home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,19 @@
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
+import usePageTitle from "../../utils/usePageTitle";
+
 import Container from "../../components/Container";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 import SearchCard from "../../components/SearchCard";
 import CampCard from "../../components/CampCard";
-
-import { PageLoading } from "../../components/Loading";
 import ListCards from "../../components/ListCards";
 
+import { PageLoading } from "../../components/Loading";
+
 const Home = () => {
+
+    usePageTitle("YelpCamp | Página Inicial");
 
     const { documents: data, error, loading } = useFetchDocuments("camps");
 
