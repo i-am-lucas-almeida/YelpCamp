8b98a3828ee4b99504d064c35aa929ba46d0bbd3
Lucas <77863766+i-am-lucas-almeida@users.noreply.github.com>
2022-07-29 20:23:30 -0300

    Create Price Mask component

---
+++ src/utils/PriceMask.js
@@ -0,0 +1,10 @@
+export const PriceMask = (e) => {
+
+    let value = e.target.value;
+    value = value.replace(/\D/g, "");
+    value = value.replace(/(\d)(\d{2})$/, "$1,$2");
+    value = value.replace(/(?=(\d{3}) + (\D))\B/g);
+    e.target.value = value;
+    return e;
+
+};
